222e2659fe8ba30d6ea9ac2ae93694ac6c2a4c92
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2019-02-18 11:15:55 +0100

    Comment

--- MikkelsSkinkeKutter/Feeder/Sausage.cs
+++ MikkelsSkinkeKutter/Feeder/Sausage.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace MikkelsSkinkeKutter.Feeder
 {
+    /// <summary>
+    /// Here you can't eat cold sausages
+    /// </summary>
     public abstract class Sausage : FoodItem
     {
         public Sausage()
